9bf824f7d30d61481a7c97ee6b8365c1c94db1de
Hakoyu <Hakoyu@outlook.com>
2026-09-12 11:56:50 +0800

    更新benchmark

--- HKW.MVVMBenchmark/Program.cs
+++ HKW.MVVMBenchmark/Program.cs
@@ -1,9 +1,21 @@
-﻿using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Running;
 
 namespace HKW.MVVMBenchmark;
 
 internal static class Program
 {
-    private static void Main(string[] args) =>
-        BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+    private static void Main(string[] args)
+    {
+        var config = DefaultConfig.Instance.WithOption(ConfigOptions.JoinSummary, true);
+
+        BenchmarkRunner.Run<BindToBenchmarks>(config, args);
+        BenchmarkRunner.Run<TwoWayBindBenchmarks>(config, args);
+        BenchmarkRunner.Run<WhenAnyValueBenchmarks>(config, args);
+        BenchmarkRunner.Run<NestedWhenAnyValueBenchmarks>(config, args);
+        BenchmarkRunner.Run<MultiPropertyWhenAnyValueBenchmarks>(config, args);
+        BenchmarkRunner.Run<ToPropertyBenchmarks>(config, args);
+        BenchmarkRunner.Run<ObservableOperatorBenchmarks>(config, args);
+    }
 }
